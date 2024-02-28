1cc4deb48272a791532db4d74bf3b689df68b63d
Javier Jiménez-Alfaro Hacha <javilin7967@gmail.com>
2024-02-28 23:09:05 +0100

    audio añadido a sala de meditación

--- app/src/main/java/com/hachatml/zenix/MainActivity.kt
+++ app/src/main/java/com/hachatml/zenix/MainActivity.kt
@@ -52,7 +52,7 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
                     NavHost(
                         navController = navController,
-                        startDestination = Routes.MainScreen.route
+                        startDestination = Routes.SessionPlaying.route
                     ) {
                         composable(Routes.SplashScreen.route) { SplashScreen(navController) }
                         composable(Routes.MainScreen.route) { MainScreen(navController,MeditationVM,googleAuthUiClient.getSignedInUser()) }
--- app/src/main/java/com/hachatml/zenix/Screens/Session Playing.kt
+++ app/src/main/java/com/hachatml/zenix/Screens/Session Playing.kt
@@ -1,6 +1,8 @@
 package com.hachatml.zenix.Screens
 
+import android.media.MediaPlayer
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,9 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.google.relay.compose.RelayColumn
+import com.hachatml.zenix.R
+import com.hachatml.zenix.Routes
 import com.hachatml.zenix.actioncards.Action
 import com.hachatml.zenix.actioncards.ActionCards
 import com.hachatml.zenix.helpbutton.HelpButton
@@ -22,16 +27,24 @@ import com.hachatml.zenix.sessionplayingcloseyoureyes.SessionPlayingText
 import com.hachatml.zenix.welcome.WelcomeText
 
 @Composable
-fun PlayingColumn(navController: NavController){
-        RelayColumn(
+fun PlayingColumn(navController: NavController) {
+    val mMediaPlayer = MediaPlayer.create(LocalContext.current, R.raw.meditation)
+    mMediaPlayer.start()
+    RelayColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF9DD1F6))
+            .padding(0.dp, 150.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SessionPlayingText(modifier = Modifier.size(447.dp, 145.dp))
+        //Spacer(modifier = Modifier.size(100.dp,100.dp))
+        ReadyButton(
             modifier = Modifier
-                .fillMaxSize()
-                .background(Color(0xFF9DD1F6)).padding(0.dp,150.dp),
-            verticalArrangement = Arrangement.SpaceBetween,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            SessionPlayingText(modifier = Modifier.size(447.dp,145.dp))
-            //Spacer(modifier = Modifier.size(100.dp,100.dp))
-            ReadyButton(modifier = Modifier.size(120.dp,55.dp), readyOrNot = ReadyOrNot.LeaveSession)
-        }
+                .size(120.dp, 55.dp)
+                .clickable { navController.navigate(Routes.MainScreen.route) },
+            readyOrNot = ReadyOrNot.LeaveSession
+        )
+    }
 }
